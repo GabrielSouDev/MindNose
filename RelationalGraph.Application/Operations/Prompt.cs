da4946d0cbe8f221798591a89ece19f072fa8aa8
Gabriel Souza <61925245+GabrielSouDev@users.noreply.github.com>
2025-04-28 01:14:04 -0300

    rafatoração e criação de nova query

--- RelationalGraph.Application/Operations/Prompt.cs
+++ RelationalGraph.Application/Operations/Prompt.cs
@@ -7,37 +7,5 @@
             Message = message;
         }
         public string Message { get; private set; }
-        public static Prompt CreateKnowledgeNode(string category, string term)
-        {
-            string prompt = @"Receba dois inputs:
-                          termo: uma palavra ou conceito
-                          categoria: o contexto ao qual o termo pertence
-                          
-                          Tarefa:
-                          - Calcule um peso (de 0 a 1) que representa a relevância do termo dentro da categoria.
-                          - Gere um resumo explicando o termo no contexto da categoria.
-                          - Identifique termos com base nesse resumo relacionado a categoria.
-                          - Se não encontrar termos relacionados relevantes, retorne apenas os mais próximossemanticamente.
-                          - Para cada termo relacionado, calcule seu peso em relação a categoria (de 0 a 1).
-                          - Para cada termo relacionado, calcule seu peso em relação ao termo original (de 0 a 1).
-                          
-                          Retorne no formato:
-                          {{
-                            ""Term"": ""<termo>"",
-                            ""Summary"": ""<explicação do termo dentro da categoria>"",
-                            ""WeigthCategoryToTerm"": 0.0,
-                            ""RelatedTerms"": [
-                              {{""Term"": ""<termo_relacionado_1>"", ""WeigthCategoryToTerm"": 0.0, ""WeigthTermToTerm"": 0.0}},
-                              {{""Term"": ""<termo_relacionado_2>"", ""WeigthCategoryToTerm"": 0.0, ""WeigthTermToTerm"": 0.0}},
-                              {{""Term"": ""<termo_relacionado_3>"", ""WeigthCategoryToTerm"": 0.0, ""WeigthTermToTerm"": 0.0}}
-                            ]
-                          }}
-                          
-                          Responda estritamente no formato JSON como mostrado acima.
-                          Input real: termo: {0} categoria: {1}";
-
-            var prompMessage = string.Format(prompt, term, category);
-            return new Prompt(prompMessage);
-        }
     }
 }
--- RelationalGraph.Application/Operations/Query.cs
+++ RelationalGraph.Application/Operations/Query.cs
@@ -12,22 +12,5 @@ namespace RelationalGraph.Application.Operations
         public string CommandLine { get; private set; }
         public object Parameters { get; private set; }
 
-        //TESTAR E CRIAR NOVAS QUERYS - TESTAR MERGE
-        public static Query CreateKnowledgeNode(TermResult termResult) =>
-            new Query(@"WITH $termResult AS initialTerm
-                        CREATE (n:Term { Term: initialTerm.Term, 
-                                         Summary: initialTerm.Summary, 
-                                         WeigthCategoryToTerm: initialTerm.WeigthCategoryToTerm, 
-                                         CreatedAt: initialTerm.CreatedAt})
-                        WITH initialTerm, n
-                        UNWIND initialTerm.RelatedTerms AS relatedTerm
-                        CREATE (r:Term { Term: relatedTerm.Term,
-                                         WeigthCategoryToTerm: relatedTerm.WeigthCategoryToTerm,
-                                         WeigthTermToTerm: relatedTerm.WeigthTermToTerm,
-                                         CreatedAt: relatedTerm.CreatedAt})
-                        CREATE (n)-[:RELATED_TO { prompt_tokens: initialTerm.Usage.prompt_tokens,
-                                                  completion_tokens: initialTerm.Usage.completion_tokens,
-                                                  total_tokens: initialTerm.Usage.total_tokens }]->(r)
-                        RETURN n", new { termResult });
     }
 }
