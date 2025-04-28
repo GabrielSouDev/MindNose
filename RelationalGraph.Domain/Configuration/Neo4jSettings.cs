be50df32e9d4854687b5647648f567fa66cec899
Gabriel Souza <61925245+GabrielSouDev@users.noreply.github.com>
2025-04-28 01:21:49 -0300

    Alterando parametrização do appsettings

--- RelationalGraph.Domain/Configuration/Neo4jSettings.cs
+++ RelationalGraph.Domain/Configuration/Neo4jSettings.cs
@@ -2,7 +2,7 @@
 
 public class Neo4jSettings
 {
-    public string Url { get; private set; } = string.Empty;
-    public string Username { get; private set; } = string.Empty;
-    public string Password { get; private set; } = string.Empty;
+    public string Url { get; init; } = string.Empty;
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
 }
